fe76d5b19213e65ac42f984a038c1dff29ca9898
Viper <rapidviperman@outlook.com>
2018-07-08 17:59:10 +0200

    fixed m1c shells

--- cod ww2 sweps/lua/weapons/cod_ww2_m1c/shared.lua
+++ cod ww2 sweps/lua/weapons/cod_ww2_m1c/shared.lua
@@ -97,7 +97,7 @@ SWEP.ShellViewAngleAlign = {Forward = 0, Right = 0, Up = 0}
 SWEP.ShellAttachmentName = "2"
 SWEP.ShellDelay = 0.01
 SWEP.ShellScale = 1
-SWEP.ShellModel = "models/pb_cod_ww2/shells/556x45.mdl"
+SWEP.ShellModel = "models/codww2/shells/556x45.mdl"
 SWEP.ShellEjectVelocity = 0
 
 SWEP.MuzzleAttachmentName = "1"
